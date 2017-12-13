e6725f6f8c1093e71d29c7789205f99f45de1c2d
Nina Tran <ninatranschacc@gmail.com>
2017-12-13 10:37:13 -0800

    Checking in a skeleton web project

---
+++ C43/Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(C43Solution.Web.Startup))]
+namespace C43Solution.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
